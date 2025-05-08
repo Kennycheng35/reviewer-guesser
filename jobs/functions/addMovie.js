21b614ba90de4060d7ccec97130bc187bab74942
Kenny Cheng <kennycheng35@gmail.com>
2025-05-08 11:25:13 -0400

    trying to improve docker scraper performance

--- jobs/functions/addMovie.js
+++ jobs/functions/addMovie.js
@@ -3,6 +3,7 @@ import {Movie} from '../models/index.js';
 export const addMovie = async (data) => {
     try {
         const movies = await Movie.bulkCreate(data, {ignoreDuplicates: true});
+        console.log("Movies added:", movies);
         return movies;
     }
     catch (e) {
